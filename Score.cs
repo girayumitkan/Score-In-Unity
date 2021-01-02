fe2a6d75cd2cd35f8ee84ffd02b6ce6075c3aed3
Giray Ümitkan <55592038+girayumitkan@users.noreply.github.com>
2021-01-02 13:22:46 +0300

    Add files via upload

---
+++ Score.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+	public static Score instance;
+	public int score,highScore;
+	public Text scoreText,highScoreText,gameOverscoreText;
+	
+	private void Awake(){
+		instance = this;
+		if(PlayerPrefs.HasKey("HighScore")){
+		highScore = PlayerPrefs.GetInt("HighScore");
+		highScoreText.text = highScore.ToString();
+		}
+		
+		if(PlayerPrefs.HasKey("Score")){
+		score = PlayerPrefs.GetInt("Score");
+		scoreText.text = score.ToString();
+		}
+	}
+	
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+	public void AddscoreEasy(){
+		score++;
+		UpdateHighScore();
+		scoreText.text = score.ToString();
+		PlayerPrefs.SetInt("Score",score);
+	}
+	public void AddscoreNormal(){
+		score = score+2;
+		UpdateHighScore();
+		scoreText.text = score.ToString();
+		PlayerPrefs.SetInt("Score",score);
+	}
+	public void AddscoreHard(){
+		score = score+3;
+		UpdateHighScore();
+		scoreText.text = score.ToString();
+		PlayerPrefs.SetInt("Score",score);
+	}
+	public void UpdateHighScore(){
+		if(score > highScore){
+			highScore = score;
+			highScoreText.text = highScore.ToString();
+			PlayerPrefs.SetInt("HighScore",highScore);
+		}
+	}
+	public void ResetScore(){
+		PlayerPrefs.DeleteKey("Score");
+		score = 0;
+		scoreText.text = score.ToString();
+	}
+}
